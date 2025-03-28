fcf34e61c6ab8f3a376e53a84f016b8c8f2dd981
Lowkah404 <lasmarkhh@gmail.com>
2025-03-28 12:13:05 -0400

    Added part 1

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -5,15 +7,54 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        ArrayList<Product> products = new ArrayList<Product>();
         Scanner scanner = FileHandler.createInputScanner("TradeData.txt");
         while(scanner.hasNextLine()){
             String name = scanner.next();
             String country = scanner.next();
             String category = scanner.next();
             double price = scanner.nextDouble();
+            Product product = new Product(name, country, category, price);
+            products.add(product);
+        }
+        for(int i = 0; i < products.size(); i++){
+            Product product = products.get(i);
+            if(product.getCountry().equals("China")){
+                product.setPrice(product.getPrice()*1.25);
+            }
+            else if(product.getCountry().equals("USA") && product.getCategory().equals("Electronics")){
+                product.setPrice(product.getPrice()*1.1);
+            }
+            else if(product.getCountry().equals("Japan") && product.getCategory().equals("Automobiles")){
+                product.setPrice(product.getPrice()*1.15);
+            }
+            else if(product.getCountry().equals("India") && product.getCategory().equals("Agriculture")){
+                product.setPrice(product.getPrice()*1.05);
+            }
+            else if(product.getCountry().equals("South Korea") && product.getCategory().equals("Electronics")){
+                product.setPrice(product.getPrice()*1.08);
+            }
+            else if(product.getCountry().equals("Saudi Arabia") && product.getCategory().equals("Energy")){
+                product.setPrice(product.getPrice()*1.12);
+            }
+            else if(product.getCountry().equals("Germany") && product.getCategory().equals("Manufacturing")){
+                product.setPrice(product.getPrice()*1.06);
+            }
+            else if(product.getCountry().equals("Bangladesh") && product.getCategory().equals("Textile")){
+                product.setPrice(product.getPrice()*1.04);
+            }
+            else if(product.getCountry().equals("Brazil") && product.getCategory().equals("Agriculture")){
+                product.setPrice(product.getPrice()*1.09);
+            }
         }
 
+        products.sort((a,b) -> a.getCountry().compareTo(b.getCountry()));
 
+        PrintWriter writer = FileHandler.createOutputPrintWriter("UpdatedTradeData.txt");
+        for(int i =0; i<products.size(); i++){
+            Product product = products.get(i);
+            writer.print(product.getName() + "," + product.getCountry() + "," + product.getCategory() + "," + product.getPrice());
+        }
 
 
 
--- src/Product.java
+++ src/Product.java
@@ -1,6 +1,47 @@
 public class Product {
+    private String name;
+    private String country;
+    private String category;
+    private double price;
 
+    public Product() {
+        this.name = "";
+        this.country = "";
+        this.category = "";
+        this.price = 0.0;
+    }
 
+    public Product(String name, String country, String category, double price) {
+        this.name = name;
+        this.country = country;
+        this.category = category;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getCountry() {
+        return country;
+    }
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
 
 
 
